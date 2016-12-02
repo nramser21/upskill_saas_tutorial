a0613853ac2fd4ff5bc7952087990fb882cc831c
nramser21 <nramser21@gmail.com>
2016-12-02 21:55:52 +0000

    Added profile create action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,4 +2,20 @@ class ProfilesController < ApplicationController
   def new
     @profile = Profile.new
   end
+  
+  def create
+    @user = User.find(params[:user_id])
+    @profile = @user.build_profile(profile_params)
+    if @profile.save
+      flash[:success] = "Profile ipdated!"
+      redirect_to root_path
+    else
+      render action: :new
+    end
+  end
+  
+  private
+    def profile_params
+      params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :email, :description)
+    end
 end
