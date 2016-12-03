7f7eee1e03c86cccc32bcb3333ca165221175d6b
nramser21 <nramser21@gmail.com>
2016-12-03 01:53:39 +0000

    Added before filters to check correct user

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,6 @@
 class ProfilesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :only_current_user
   def new
     @profile = Profile.new
   end
@@ -31,5 +33,10 @@ class ProfilesController < ApplicationController
   private
     def profile_params
       params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :email, :description, :avatar)
+    end
+    
+    def only_current_user
+      @user = User.find(params[:user_id])
+      redirect_to(root_url) unless @user == current_user
     end
 end
