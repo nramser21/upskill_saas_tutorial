951ad54d0eecf554101d7854dab04ad6606089eb
nramser21 <nramser21@gmail.com>
2016-12-03 01:47:07 +0000

    Added update action for user profiles

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -8,7 +8,7 @@ class ProfilesController < ApplicationController
     @profile = @user.build_profile(profile_params)
     if @profile.save
       flash[:success] = "Profile updated!"
-      redirect_to user_path(params[:user_id])
+      redirect_to user_path(id: params[:user_id])
     else
       render action: :new
     end
@@ -16,6 +16,16 @@ class ProfilesController < ApplicationController
   
   def edit
     @profile = User.find(params[:user_id]).profile
+  end
+  
+  def update
+    @profile = User.find(params[:user_id]).profile
+    if @profile.update_attributes(profile_params)
+      flash[:success] = "Profile has been updated"
+      redirect_to user_path(id: params[:user_id])
+    else
+      render action: :edit
+    end
   end
     
   private
